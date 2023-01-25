10dd5423cc30f221216da5b766809a4b24271789
lol lol <loromate2018@outlook.com>
2023-01-25 23:17:32 +0300

    vbmjhvcmhfvc

--- prak_G_13_Client/MainWindow.xaml.cs
+++ prak_G_13_Client/MainWindow.xaml.cs
@@ -26,30 +26,35 @@ namespace prak_G_13_Client
     {
         public MainWindow()
         {
-            Version latestVersion = Get_Latest_Version().Result;
-            var currentversion = Assembly.GetExecutingAssembly().GetName().Version;
-            if (currentversion < latestVersion)//TODO <
+            try
             {
-                var tempPath = System.IO.Path.GetTempPath();
-                var tempProjPath = tempPath + "prak_G_13";
-                var updaterPath = tempProjPath + "\\Updater.exe";
-                Directory.CreateDirectory(tempProjPath);
-                var ress = GetType().Assembly.GetManifestResourceNames();
-                using (Stream resource = new MemoryStream(Properties.Resources.Updater))
+                Version latestVersion = Get_Latest_Version().Result;
+                var currentversion = Assembly.GetExecutingAssembly().GetName().Version;
+                if (currentversion < latestVersion)//TODO <
                 {
-                    if (resource == null)
+                    var tempPath = System.IO.Path.GetTempPath();
+                    var tempProjPath = tempPath + "prak_G_13";
+                    var updaterPath = tempProjPath + "\\Updater.exe";
+                    Directory.CreateDirectory(tempProjPath);
+                    var ress = GetType().Assembly.GetManifestResourceNames();
+                    using (Stream resource = new MemoryStream(Properties.Resources.Updater))
                     {
-                        throw new ArgumentException("No such resource", "resourceName");
+                        if (resource == null)
+                        {
+                            throw new ArgumentException("No such resource", "resourceName");
+                        }
+                        using (Stream output = File.OpenWrite(updaterPath))
+                        {
+                            resource.CopyTo(output);
+                        }
                     }
-                    using (Stream output = File.OpenWrite(updaterPath))
-                    {
-                        resource.CopyTo(output);
-                    }
-                }
-                Process.Start(updaterPath, new List<string>() { Directory.GetCurrentDirectory() + "\\prak_G_13_Client.exe" });
-                this.Close();
+                    Process.Start(updaterPath, new List<string>() { Directory.GetCurrentDirectory() + "\\prak_G_13_Client.exe" });
+                    this.Close();
 
+                }
             }
+            catch { }
+            
             InitializeComponent();
         }
         private async Task<Version> Get_Latest_Version()
