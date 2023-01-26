5388dc15fc97d977b1685fabfe8f2ccaa180d5b6
lol lol <loromate2018@outlook.com>
2023-01-26 10:36:44 +0300

    dsfdsfrfrf

--- prak_G_13_Client/obj/Release/net6.0-windows/prak_G_13_Client.AssemblyInfo.cs
+++ prak_G_13_Client/obj/Release/net6.0-windows/prak_G_13_Client.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("prak_G_13_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.0.7")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.0.8")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("prak_G_13_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prak_G_13_Client")]
-[assembly: System.Reflection.AssemblyVersionAttribute("0.0.0.7")]
+[assembly: System.Reflection.AssemblyVersionAttribute("0.0.0.8")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
