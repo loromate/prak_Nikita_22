9683ff3eb35b70d1515c2c03e7ca0d5677ee17f2
lol lol <loromate2018@outlook.com>
2023-01-26 11:02:58 +0300

    njhvubhvkhj

--- prak_G_13_Client/Properties/Resources.Designer.cs
+++ prak_G_13_Client/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace prak_G_13_Client.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace prak_G_13_Client.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("prak_G_13_Client.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace prak_G_13_Client.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,9 +63,9 @@ namespace prak_G_13_Client.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
-        internal static byte[] egr {
+        public static byte[] Updater {
             get {
-                object obj = ResourceManager.GetObject("egr", resourceCulture);
+                object obj = ResourceManager.GetObject("Updater", resourceCulture);
                 return ((byte[])(obj));
             }
         }
@@ -73,9 +73,9 @@ namespace prak_G_13_Client.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
-        internal static byte[] Updater {
+        public static byte[] witch_doctor {
             get {
-                object obj = ResourceManager.GetObject("Updater", resourceCulture);
+                object obj = ResourceManager.GetObject("witch_doctor", resourceCulture);
                 return ((byte[])(obj));
             }
         }
